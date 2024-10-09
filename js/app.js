b90aa8f0128c2daaf79bb0f4684e496dd4ae74fd
fatemnhm <fatemanhm@gmail.com>
2024-10-09 16:28:26 +0300

    update

--- js/app.js
+++ js/app.js
@@ -203,6 +203,17 @@ const cases = [
 
 
 
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+
 // Random case
 function randomCase() {
     const randomIndex = Math.floor(Math.random() * cases.length);
@@ -389,101 +400,8 @@ start.addEventListener('click', (event) => {
    start.classList.add('hidden');
    game.classList.remove('hidden');
   
- });
+ })
 
-
-//function getCase(){
-
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//onst startGame =document.querySelector('#start');
-//const gameBody=document.querySelector('#game')
-
-
-//startGame.addEventListener('click',()=>{startPlay()});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//startPlay(){
-    //startGame.classList.add('hidden');
-    //gameBody.classList.remove('hidden');
-
-//}
-
-
-
-
-
-
-
-
-   
-
-
-
-// // Step 1: Add an event listener to the "Post" button
-// document.getElementById("btn-post").addEventListener("click", function () {
-//     // This line sets up an event listener for the click event on the "Post" button.
-//     // When the button is clicked, the function provided as the second argument will run.
-
-//     // Step 2: Get the comment from the textarea
-//     const newComment = document.getElementById("new-comment").value;
-
-//     // Step 3: Create a new paragraph element for the comment
-//     const commentContainer = document.getElementById("comment-container");
-//     const commentElement = document.createElement("p");
-//     commentElement.innerText = newComment;
-
-//     // Step 4: Append the comment to the comment container
-//     commentContainer.appendChild(commentElement);
-
-//     // Clear the textarea after posting the comment
-//     document.getElementById("new-comment").value = "";
-// });
-
-
-
-
-
-
-/*---------- Variables (state) ---------*/
-
-
-/*----- Cached Element References  -----*/
-
-
-/*-------------- Functions -------------*/
 
 
 /*----------- Event Listeners ----------*/
